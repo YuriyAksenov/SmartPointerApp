d1af13d0acaf4ad21930edb3a83996ec4c5abd4d
Yuriy Aksenov <Yura290610@yandex.ru>
2017-10-04 16:26:48 +0300

    Добавлен пример использования

--- SmartPointerApp/SmartPointer.h
+++ SmartPointerApp/SmartPointer.h
@@ -1,10 +1,11 @@
-#pragma once
+#ifndef SMART_POINTER_H
+#define SMART_POINTER_H
 
 template<typename T>
 class sm_ptr
 {
 private:
-	T* m_obj nullptr;
+	T* m_obj = nullptr;
 public:
 	sm_ptr(T* obj) : m_obj(obj) {}
 	~sm_ptr() {
@@ -13,5 +14,6 @@ public:
 
 	T* operator ->() { return m_obj; }
 	T& operator * () { return *m_obj; }
-
 };
+
+#endif SMART_POINTER_H
--- SmartPointerApp/SmartPointerApp.cpp
+++ SmartPointerApp/SmartPointerApp.cpp
@@ -2,10 +2,44 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include "SmartPointer.h"
+
+using namespace std;
+
+
+
+class Cfoo
+{
+public:
+	Cfoo();
+	~Cfoo();
+	void counter() {
+		k++;
+	}
+	int k;
+};
+
+Cfoo::Cfoo() :k(0)
+{
+	cout << "Konstructor.  | This class was creted by Smart Pointer.";
+}
+
+Cfoo::~Cfoo()
+{
+	cout << "Destructor. | This class was deleted automatically by Smart Pointer.";
+}
 
 
 int main()
 {
+	sm_ptr<Cfoo> cfoo(new Cfoo());
+	
+	cout << "\n---------------\n";
+	cout << "k=" << cfoo->k << endl;
+	cfoo->counter();
+	cout << "k=" << cfoo->k << endl;
+	cout << "\n---------------\n";
     return 0;
 }
 
